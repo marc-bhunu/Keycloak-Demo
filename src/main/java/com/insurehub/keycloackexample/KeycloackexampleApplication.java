d1dd449cbaceea74cfeb8962d1e5e373d233eb18
marc-bhunu <64252485+marc-bhunu@users.noreply.github.com>
2021-03-23 23:18:20 +0200

    Init

---
+++ src/main/java/com/insurehub/keycloackexample/KeycloackexampleApplication.java
@@ -0,0 +1,13 @@
+package com.insurehub.keycloackexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/insurehub/keycloackexample/KeycloackexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurehub.keycloackexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
